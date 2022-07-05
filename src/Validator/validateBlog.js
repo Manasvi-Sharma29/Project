6ac8bfef6b3b945a7cb63e033d36296c45d7cee6
Manasvi-Sharma29 <manasvi.sharma2929@gmail.com>
2022-07-05 19:14:47 +0530

    Completed

--- src/Validator/validateBlog.js
+++ src/Validator/validateBlog.js
@@ -5,16 +5,19 @@ const authorModel = require("../models/authorModel");
 const checkValue = function (value) {
   let arrValue = [];
   value.map((x) => {
-    if (x.trim().length) arrValue.push(x);
+    x= x.trim();
+    if (x.length) arrValue.push(x);
   });
   return arrValue.length ? arrValue : false;
 };
 
-// function for converting string into array
+//function for converting string into array
 const convertToArray = function (value) {
-  if (typeof value == "string" && value) {
-    if (value.trim().length == 0) return false;
-    return [value];
+  if (typeof value == "string") {
+    if(value.trim()){
+    let newValue = value.trim()
+    return [newValue];
+    }
   } else if (value?.length > 0) return checkValue(value);
   return false;
 };
